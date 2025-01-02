a2d5ade97ed36c1f50f48dc1a34adcf54ac005a5
mbalashovv <isdedtool@gmail.com>
2025-01-02 14:26:13 +0300

    [*] Fixed search errors & Removed unneccessary code & Refactored & Recompiled app for windows & Adjusted README.md

--- go.mod
+++ go.mod
@@ -1,10 +1,13 @@
 module parser
 
-go 1.20
+go 1.23
 
 require (
+	github.com/PuerkitoBio/goquery v1.10.1 // indirect
+	github.com/andybalholm/cascadia v1.3.3 // indirect
 	github.com/fatih/color v1.15.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
-	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/net v0.33.0 // indirect
+	golang.org/x/sys v0.28.0 // indirect
 )
--- services/services.go
+++ services/services.go
@@ -2,229 +2,113 @@ package services
 
 import (
 	"fmt"
-	"io"
 	"log"
 	"net/http"
 	"strings"
 
+	"github.com/PuerkitoBio/goquery"
 	"github.com/fatih/color"
 )
 
-func GetTitle(url string) {
-	resp, err := http.Get(url)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	body, _ := io.ReadAll(resp.Body)
-	strBody := string(body)
-	startTitle := strings.Index(strBody, "<title>")
-	//Check if startTitle equals -1 then there is no title
-	if startTitle == -1 {
-		fmt.Println("There is no title")
-		return
-	}
-	startTitle += 7
-	endTitle := strings.Index(strBody, "</title>")
-	pageTitle := []byte(strBody[startTitle:endTitle])
-	fmt.Printf("Page title: %s\n", pageTitle)
-}
-
-func GetWordCount(url, word string) {
-	//Make all words lowercase
-	word = strings.ToLower(word)
-	resp, err := http.Get(url)
-	if err != nil {
-		log.Fatal(err)
-	}
-	body, er := io.ReadAll(resp.Body)
-	if er != nil {
-		log.Fatal(er)
-	}
-	data := strings.ToLower(string(body))
-	count := strings.Count(data, word)
-	fmt.Printf("%s - %s: %d\n", url, word, count)
-}
-
 func changeWord(str string) string {
-	//Converts russian letters into english ones
 	letterDict := map[rune]string{
-		'а': "a",
-		'б': "b",
-		'в': "v",
-		'г': "g",
-		'д': "d",
-		'е': "e",
-		'ё': "yo",
-		'ж': "zh",
-		'з': "z",
-		'и': "i",
-		'й': "y",
-		'к': "k",
-		'л': "l",
-		'м': "m",
-		'н': "n",
-		'о': "o",
-		'п': "p",
-		'р': "r",
-		'с': "s",
-		'т': "t",
-		'у': "u",
-		'ф': "f",
-		'х': "h",
-		'ц': "c",
-		'ч': "ch",
-		'ш': "sh",
-		'щ': "sh",
-		'ы': "i",
-		'э': "e",
-		'ю': "yu",
-		'я': "ya",
-		' ': "-",
-		'ъ': "",
-		'ь': "",
+		'а': "a", 'б': "b", 'в': "v", 'г': "g", 'д': "d",
+		'е': "e", 'ё': "yo", 'ж': "zh", 'з': "z", 'и': "i",
+		'й': "y", 'к': "k", 'л': "l", 'м': "m", 'н': "n",
+		'о': "o", 'п': "p", 'р': "r", 'с': "s", 'т': "t",
+		'у': "u", 'ф': "f", 'х': "h", 'ц': "c", 'ч': "ch",
+		'ш': "sh", 'щ': "sh", 'ы': "i", 'э': "e", 'ю': "yu",
+		'я': "ya", ' ': "-", 'ъ': "", 'ь': "",
 	}
-	str = strings.ToLower(str)
+	str = strings.TrimSpace(strings.ToLower(str))
 
-	newStr := ""
-
+	var newStr strings.Builder
 	for _, v := range str {
-		newStr += letterDict[v]
+		if value, exists := letterDict[v]; exists {
+			newStr.WriteString(value)
+		} else {
+			newStr.WriteString(string(v))
+		}
 	}
-	return newStr
+	return newStr.String()
 }
 
-func getAttr(body, attr string) string {
-	var attrStart int
-	var attrEnd int
+func GetInfo(nameToFind string) {
+	// transliterate russian name to english one
+	nameToFind = changeWord(nameToFind)
 
-	switch attr {
-	case "height":
-		attrStart = strings.Index(body, "см")
-		attrStart -= 4
-		newBody := body[attrStart:]
-		attrEnd = strings.Index(newBody, "\"")
-		return string(newBody[:attrEnd])
-	case "age":
-		attrStart = strings.Index(body, "году")
-		attrStart += 57
-		newBody := body[attrStart:]
-		attrEnd = strings.Index(newBody, ")")
-		return string(newBody[:attrEnd])
-	case "work":
-		work := ""
-		attrStart = strings.Index(body, "idstag")
-		body = body[attrStart:]
-		for strings.Index(body, "idstag") != -1 {
-			attrStart = strings.Index(body, "idstag")
-			attrStart = strings.Index(body[attrStart:], ">") + 1
-			body = body[attrStart:]
-			attrEnd = strings.Index(body, "<")
-			work += string(body[:attrEnd]) + " "
-			body = body[attrEnd+16:]
-		}
+	url := fmt.Sprintf("https://uznayvse.ru/znamenitosti/biografiya-%s.html", nameToFind)
 
-		// return strings.Replace(strings.Trim(work, " "), " ", ", ", -1)
-		return work
-	case "zodiac":
-		attrStart = strings.Index(body, "{\"gen_type\":\"zodiac\"}")
-		attrStart += 23
-		newBody := body[attrStart:]
-		attrEnd = strings.Index(newBody, "<")
-		return string(newBody[:attrEnd])
-	case "likes":
-		attrStart = strings.Index(body, "data-positive")
-		attrStart += 15
-		newBody := body[attrStart:]
-		attrEnd = strings.Index(newBody, "\"")
-		return fmt.Sprintf("\x1b[%dm%s\x1b[0m", 92, newBody[:attrEnd])
-	case "dislikes":
-		attrStart = strings.Index(body, "data-negative")
-		attrStart += 15
-		newBody := body[attrStart:]
-		attrEnd = strings.Index(newBody, "\"")
-		return fmt.Sprintf("\x1b[%dm%s\x1b[0m", 91, newBody[:attrEnd])
-	default:
-		attrStart = strings.Index(body, attr)
-		//checking if patrynomic is none
-		if attrStart == -1 {
-			return "-"
-		}
-		attrStart += len(attr) + 2
-		newBody := body[attrStart:]
-		attrEnd = strings.Index(newBody, "<")
-		return string(newBody[:attrEnd])
-	}
-
-}
-
-func GetInfo(name string) {
-	name = changeWord(name)
-	url := fmt.Sprintf("https://uznayvse.ru/znamenitosti/biografiya-%s.html", name)
 	resp, err := http.Get(url)
 	if err != nil {
 		fmt.Println(err)
 		return
 	} else if resp.StatusCode == 404 {
-		fmt.Printf("%s не был найден\n", name)
+		fmt.Printf("%s не был найден\n", nameToFind)
 		return
 	}
-	defer resp.Body.Close()
-	body, _ := io.ReadAll(resp.Body)
-	startBody := strings.Index(string(body), "http://schema.org/Person")
-	endBody := strings.Index(string(body), "action_block main_shadow bo_ra tablecontents")
-	newBody := string(body)[startBody:endBody]
-	nick := getAttr(newBody, "name")
-	// realName := getAttr(newBody, "givenName")
-	patrynomic := getAttr(newBody, "additionalName")
-	fullname := "Полное имя: " + nick + " " + patrynomic
-	weight := "Вес: " + getAttr(newBody, "weight")
-	height := "Рост: " + getAttr(newBody, "height")
-	city := "Город: " + getAttr(newBody, "birthPlace")
-	age := "Возраст: " + getAttr(newBody, "age")
-	work := "Вид деятильности: " + getAttr(newBody, "work")
-	zodiac := "Знак зодиака: " + getAttr(newBody, "zodiac")
-	likes := "Нравится: " + getAttr(newBody, "likes")
-	dislikes := "Не нравится: " + getAttr(newBody, "dislikes")
 
-	sex := ""
-	//checking if last letter in patrynomic equals russian "а" letter
-	if patrynomic[len(patrynomic)-1] != 176 {
-		sex = "boy"
+	defer resp.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	infoBox := doc.Find(`div[itemtype="http://schema.org/Person"]`)
+
+	// Extract data using selectors
+
+	// Name could be a list of elements, therefore check it for lenght
+	nameElement := infoBox.Find("span[itemprop='name']")
+	name := ""
+	if nameElement.Length() > 1 {
+		name = nameElement.First().Text()
 	} else {
-		sex = "girl"
+		name = nameElement.Text()
 	}
 
-	//Call the function which obtains and returns formatted data
-	wrapper(fullname, height, weight, city, age, work, zodiac, likes, dislikes, sex)
+	patrynomic := infoBox.Find("strong[itemprop='additionalName']").Text()
+	profession := "Вид деятельности: " + strings.TrimSpace(infoBox.Find("dt:contains('Кто такой:')").Next().Find("strong").Text())
+	birthDate := "Датя рождения: " + infoBox.Find("meta[itemprop='birthDate']").AttrOr("content", "N/A")
+	ageParts := strings.Split(infoBox.Find("dt:contains('День рождения:')").Next().Text(), "(")
+	age := "Возраст: " + strings.TrimSuffix(strings.TrimSpace(ageParts[len(ageParts)-1]), ")")
+	city := "Город: " + infoBox.Find("strong[itemprop='birthPlace']").Text()
+	height := "Рост: " + infoBox.Find("strong[itemprop='height']").Text()
+	weight := "Вес: " + infoBox.Find("strong[itemprop='weight']").Text()
+	zodiac := "Знак зодиака: " + infoBox.Find("dt:contains('Знак Зодиака:')").Next().Find("a").First().Text()
+	likes := "Нравится: " + infoBox.Find("[data-positive]").AttrOr("data-positive", "-")
+	dislikes := "Не нравится: " + infoBox.Find("[data-negative]").AttrOr("data-negative", "-")
+
+	fullname := "Полное имя: " + name + " " + patrynomic
+
+	sex := "male"
+	//checking if the last letter in patrynomic equals russian "а" letter
+	if len(patrynomic) > 0 && patrynomic[len(patrynomic)-1] == 176 {
+		sex = "female"
+	}
+
+	wrapper([]interface{}{fullname, height, weight, city, age, birthDate, profession, zodiac, likes, dislikes}, sex)
 }
 
-func wrapper(data ...interface{}) {
+func wrapper(data []interface{}, sex string) {
 	var c *color.Color
-	if data[len(data)-1] == "boy" {
-		//stands for blue
+	if sex == "male" {
 		c = color.New(color.FgHiBlue)
 	} else {
-		//stands for pink
 		c = color.New(color.FgHiMagenta)
 	}
-	//returns colored "#" symbol which depends on sex
+
 	coloredSharp := c.Sprint("#")
 	fmt.Println()
 	fmt.Println(strings.Repeat(coloredSharp, 60))
 	fmt.Printf("%s\n", coloredSharp)
 
-	for i, d := range data {
-		if i == len(data)-1 {
-			break
-		}
-		//Padding from left side
+	for _, d := range data {
 		padding := 5
 		fmt.Printf("%s%s%v\n", coloredSharp, strings.Repeat(" ", padding), d)
 		fmt.Printf("%s\n", coloredSharp)
 	}
 
 	fmt.Println(strings.Repeat(coloredSharp, 60))
-	fmt.Println()
 	fmt.Println()
 }
